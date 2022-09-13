94027b607bfbd315f524d8946f0c1280a8ef21b8
Neo Trinity <84890228+NeoTrinity-FF14@users.noreply.github.com>
2022-09-13 10:07:18 +0900

    Update config.h

--- lhp14c_latest_qmk/config.h
+++ lhp14c_latest_qmk/config.h
@@ -56,13 +56,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  /*== all animations enable ==*/
 //   #define RGBLIGHT_ANIMATIONS
  /*== or choose animations ==*/
-   #define RGBLIGHT_EFFECT_BREATHING
+   //#define RGBLIGHT_EFFECT_BREATHING
    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-   #define RGBLIGHT_EFFECT_SNAKE
-   #define RGBLIGHT_EFFECT_KNIGHT
-   #define RGBLIGHT_EFFECT_CHRISTMAS
-   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+   //#define RGBLIGHT_EFFECT_SNAKE
+   //#define RGBLIGHT_EFFECT_KNIGHT
+   //#define RGBLIGHT_EFFECT_CHRISTMAS
+   //#define RGBLIGHT_EFFECT_STATIC_GRADIENT
    #define RGBLIGHT_EFFECT_RGB_TEST
    #define RGBLIGHT_EFFECT_ALTERNATING
  #endif
@@ -77,3 +77,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define OLED_FONT_H "keyboards/lhp14c/glcdfont_lhp14.c"
 
+#define LAYER_STATE_32BIT

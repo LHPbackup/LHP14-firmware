749bb7217aeb98832a403222397f52ed05ff717c
LHP14 <silver@clock.ocn.ne.jp>
2022-12-03 23:09:44 +0900

    Update config.h

--- lhp14f/config.h
+++ lhp14f/config.h
@@ -71,7 +71,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #define JOYSTICK_BUTTON_COUNT 1
-#define JOYSTICK_AXES_COUNT 2
+#define JOYSTICK_AXIS_COUNT 2
 
 #define OLED_TIMEOUT 0
 

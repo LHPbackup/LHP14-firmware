efcf864d0adf705925c35a28b0ff471313059406
LHP14 <silver@clock.ocn.ne.jp>
2022-12-06 20:23:38 +0900

    Update config.h

--- lhp14f/config.h
+++ lhp14f/config.h
@@ -46,6 +46,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DI_PIN D3
 #define RGBLED_NUM 28 // Number of LEDs. Tape LED >>> RGBLED_NUM 6   no LED >>> RGBLED_NUM 0
 
+#define LED_LAYOUT( \
+    L00, L01, L02, L03, L04, L05, L06, \
+    L07, L08, L09, L10, L11, L12, L13, \
+    L14, L15, L16, L17, L18, L19, L20, \
+    L21, L22, L23, L24, L25, L26, L27) \
+   { \
+    L00, L01, L02, L03, L04, L05, L06, \
+    L07, L08, L09, L10, L11, L12, L13, \
+    L14, L15, L16, L17, L18, L19, L20, \
+    L21, L22, L23, L24, L25, L26, L27  \
+   }
+
+#define RGBLIGHT_LED_MAP LED_LAYOUT( \
+  6,  5,  4,   3,  2,  1,  0,   \
+  7,  8,  9,  10, 11, 12, 13,   \
+  20, 19, 18, 17, 16, 15, 14,   \
+  21, 22, 23, 24, 25, 26, 27)   
+
  #ifdef RGB_DI_PIN
    #define RGBLIGHT_HUE_STEP 8
    #define RGBLIGHT_SAT_STEP 8
@@ -58,8 +76,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
    //#define RGBLIGHT_EFFECT_BREATHING
    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-   //#define RGBLIGHT_EFFECT_SNAKE
-   //#define RGBLIGHT_EFFECT_KNIGHT
+   #define RGBLIGHT_EFFECT_SNAKE
+   #define RGBLIGHT_EFFECT_KNIGHT
    //#define RGBLIGHT_EFFECT_CHRISTMAS
    //#define RGBLIGHT_EFFECT_STATIC_GRADIENT
    #define RGBLIGHT_EFFECT_RGB_TEST

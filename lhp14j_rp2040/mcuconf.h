1c18de994cdf0eeca311f8d856ef3042af83d659
LHP14 <102971531+LHPbackup@users.noreply.github.com>
2023-01-29 19:26:17 +0900

    Create mcuconf.h

---
+++ lhp14j_rp2040/mcuconf.h
@@ -0,0 +1,17 @@
+// Copyright 2022 Neo Trinity
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include_next <mcuconf.h>
+
+#undef RP_I2C_USE_I2C0
+#define RP_I2C_USE_I2C0 FALSE
+#undef RP_I2C_USE_I2C1
+#define RP_I2C_USE_I2C1 TRUE
+
+#undef RP_ADC_USE_ADC1
+#define RP_ADC_USE_ADC1 TRUE
+
+#undef RP_IRQ_ADC1_PRIORITY
+#define RP_IRQ_ADC1_PRIORITY 2

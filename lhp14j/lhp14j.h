83f3eb93d5e7a25ede1ce3586222815d75f05500
LHP14 <102971531+LHPbackup@users.noreply.github.com>
2024-03-14 22:04:57 +0900

    Update lhp14j.h

--- lhp14j/lhp14j.h
+++ lhp14j/lhp14j.h
@@ -2,23 +2,6 @@
 
 #include "quantum.h"
 
-// Used to create a keymap using only KC_ prefixed keys
-
-#define LAYOUT( \
-	K00, K01, K02, K03, K04, K05, K06,  \
-	K10, K11, K12, K13, K14, K15, K16,  \
-	K20, K21, K22, K23, K24, K25, K26,  \
-	K30, K31, K32, K33, K34,      K38,  \
-	                         K35, K37,  \
-	                              K36   \
-	) \
-	{ \
-	{ K00, K01, K02, K03, K04, K05, K06, KC_NO, KC_NO }, \
-	{ K10, K11, K12, K13, K14, K15, K16, KC_NO, KC_NO }, \
-	{ K20, K21, K22, K23, K24, K25, K26, KC_NO, KC_NO }, \
-	{ K30, K31, K32, K33, K34, K35, K36, K37,   K38   } \
-	}
-
 
 
 

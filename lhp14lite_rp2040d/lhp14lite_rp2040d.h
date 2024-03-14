8af7fa3a84f7807e181639c83fe2c56316f4fee0
LHP14 <102971531+LHPbackup@users.noreply.github.com>
2024-03-14 22:05:44 +0900

    Update lhp14lite_rp2040d.h

--- lhp14lite_rp2040d/lhp14lite_rp2040d.h
+++ lhp14lite_rp2040d/lhp14lite_rp2040d.h
@@ -5,21 +5,6 @@
 
 #include "quantum.h"
 
-// Used to create a keymap using only KC_ prefixed keys
-
-#define LAYOUT( \
-	K00, K01, K02, K03, K04,  \
-	K10, K11, K12, K13, K14,  \
-	K20, K21, K22, K23, K24,  \
-	K30, K31, K32, K33, K34, K35, K36   \
-	) \
-	{ \
-	{ K00, K01, K02, K03, K04, KC_NO, KC_NO }, \
-	{ K10, K11, K12, K13, K14, KC_NO, KC_NO }, \
-	{ K20, K21, K22, K23, K24, KC_NO, KC_NO }, \
-	{ K30, K31, K32, K33, K34, K35,   K36   } \
-	}
-
 
 
 
